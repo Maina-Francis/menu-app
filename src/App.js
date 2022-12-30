a08ecce48901c9073a7222d7edb969e698bf7a63
Francis Maina <sebastianfrancix@gmail.com>
2022-12-30 23:41:30 +0300

    added dynamic category filter button

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 
+// functionality for only getting unique list of categories
+const uniqueCategories = [
+  "all",
+  ...new Set(items.map((item) => item.category)),
+];
+// console.log(uniqueCategories);
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(uniqueCategories);
+
+  // Menu category filter function
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
@@ -15,7 +33,7 @@ function App() {
         </div>
         <div className="underline"></div>
 
-        <Categories />
+        <Categories filterItems={filterItems} categories={categories} />
         <Menu items={menuItems} />
       </section>
     </main>
